fec2063e3d7072212a42605cafc3986f6d3f95ef
LuiiVasquez <luivasque95@gmail.com>
2022-11-13 22:08:30 -0600

    deleting tasks - create context

--- react-tasks-application/src/components/TaskList.jsx
+++ react-tasks-application/src/components/TaskList.jsx
@@ -1,6 +1,6 @@
 import TaskCard from './TaskCard';
 
-function TaskList({tasks}) {
+function TaskList({tasks, deleteTask}) {
 
   if (tasks.length === 0) {
     return <h1>No hay tareas aun</h1>;
@@ -9,7 +9,7 @@ function TaskList({tasks}) {
   return (
     <div>
       {tasks.map((task) => (
-        <TaskCard key={task.id} task={task}/>
+        <TaskCard key={task.id} task={task} deleteTask={deleteTask}/>
       ))}
     </div>
   );
