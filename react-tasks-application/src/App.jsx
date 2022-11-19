7b88bbe44dc098e804d6ceb37f6e0fb0e4dcf039
LuiiVasquez <luivasque95@gmail.com>
2022-11-18 23:05:11 -0600

    useContenct - tailwindCSS

--- react-tasks-application/src/App.jsx
+++ react-tasks-application/src/App.jsx
@@ -1,35 +1,15 @@
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
 import { useState, useEffect } from "react";
-import { tasks as data } from "./data/tasks";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    setTasks(data);
-  }, []);
-
-  function createTask(task) {
-    setTasks([
-      ...tasks,
-      {
-        title: task.title,
-        id: tasks.length,
-        description: task.description,
-      },
-    ]);
-  }
-
-  function deleteTask(taskId) {
-    setTasks(tasks.filter(task => task.id !== taskId))
-  }
-
   return (
-    <div>
-      <TaskForm createTask={createTask} />
-      <TaskList tasks={tasks} deleteTask={deleteTask} />
-    </div>
+    <main className="bg-zinc-900 h-screen">
+      <div className="container mx-auto p-10">
+        <TaskForm />
+        <TaskList />
+      </div>
+    </main>
   );
 }
 
--- react-tasks-application/src/components/TaskList.jsx
+++ react-tasks-application/src/components/TaskList.jsx
@@ -1,15 +1,18 @@
-import TaskCard from './TaskCard';
+import TaskCard from "./TaskCard";
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
 
-function TaskList({tasks, deleteTask}) {
+function TaskList() {
+  const { tasks } = useContext(TaskContext);
 
   if (tasks.length === 0) {
-    return <h1>No hay tareas aun</h1>;
+    return <h1 className="text-white text-4xl font-bold text-center">No hay tareas aun</h1>;
   }
 
   return (
-    <div>
+    <div className="grid grid-cols-4 gap-2">
       {tasks.map((task) => (
-        <TaskCard key={task.id} task={task} deleteTask={deleteTask}/>
+        <TaskCard key={task.id} task={task} />
       ))}
     </div>
   );
