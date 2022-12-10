834961d380b325a9feb57878a11d28bfc8878bbf
LuiiVasquez <luivasque95@gmail.com>
2022-12-10 15:25:38 -0600

    adding project #2

---
+++ freecodecamp/contador-clicks/src/components/Boton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Boton.css";
+
+function Boton({ texto, esBotonClic, manejarClic }) {
+  return (
+    <button
+      className={esBotonClic ? "boton-clic" : "boton-reiniciar"}
+      onClick={manejarClic}
+    >
+      {texto}
+    </button>
+  );
+}
+
+export default Boton;
---
+++ freecodecamp/contador-clicks/src/components/Contador.jsx
@@ -0,0 +1,7 @@
+import '../styles/Contador.css'
+
+function Contador({ numClics }) {
+  return <div className="contador">{numClics}</div>;
+}
+
+export default Contador;
