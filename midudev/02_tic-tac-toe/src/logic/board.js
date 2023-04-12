44e6d3da685f3778950095069bb8c22fd59ddb44
vasquezluis <luivasquez95@gmail.com>
2023-04-11 18:31:38 -0600

    added canva-confetti module, logic and components sepated

---
+++ midudev/02_tic-tac-toe/src/logic/board.js
@@ -0,0 +1,22 @@
+import { WINNER_COMBOS } from '../constants'
+
+export const checkWinner = (boardToCheck) => {
+  // revisar todas las combinaciones ganadoras
+  // para ver si X y O gano
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo
+    if (boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c]) {
+      return boardToCheck[a] // devuelve x u o
+    }
+  }
+
+  // si no hay ganador
+  return null
+}
+
+export const checkEndGame = (newBoard) => {
+  // revisamos si hay un empate
+  // si no hay mas espacios vacios
+  // en el tablero
+  return newBoard.every((square) => square !== null)
+}
