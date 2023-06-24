ca1c29982eb43698bb5da8a82f15383f2cfc2133
vasquezluis <luivasquez95@gmail.com>
2023-06-24 15:03:36 -0600

    re structure folders

--- typescript/04_tiparArrowFunction.ts
+++
@@ -1,9 +0,0 @@
-// tipar arrow function
-
-const sumar = (a: number, b: number): number => {
-  return a + b;
-};
-
-const restar: (a: number, b: number) => number = (a, b) => {
-  return a - b;
-};
--- typescript/06_objetos.ts
+++
@@ -1,8 +0,0 @@
-// object in typescript
-
-let hero = {
-  name: 'Ironman',
-  age: 45,
-}
-
-hero.power = 100
